33d5d42896b2d6b1dd873d1a2b7a803ff2b2d7bd
Ciwan Ceylan <ciwan.ceylan@outlook.com>
2025-04-22 17:36:47 +0200

    fix: dataset names in run scripts

--- run_scripts/graph_alignment_analysis.sh
+++ run_scripts/graph_alignment_analysis.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python experiments/graph_alignment_per_node_analysis.py --dataset arenas --undirected 1 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
-python experiments/graph_alignment_per_node_analysis.py --dataset ppi_small --undirected 1 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
+python experiments/graph_alignment_per_node_analysis.py --dataset ppi --undirected 1 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
 python experiments/graph_alignment_per_node_analysis.py --dataset magna --undirected 1 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
 python experiments/graph_alignment_per_node_analysis.py --dataset enron_na --undirected 0 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
 python experiments/graph_alignment_per_node_analysis.py --dataset polblogs --undirected 0 --methods detailed_ga_analysis --noise-p 0.15 --pp-mode none::whiten --dims 512
--- run_scripts/singular_value_spectra_gnn_versions.sh
+++ run_scripts/singular_value_spectra_gnn_versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 python experiments/embedding_info_analysis.py  --dataset arenas --weighted 0 --node-attributed 1 --undirected 1 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
-python experiments/embedding_info_analysis.py  --dataset ppi_small --weighted 0 --node-attributed 1 --undirected 1 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
+python experiments/embedding_info_analysis.py  --dataset ppi --weighted 0 --node-attributed 1 --undirected 1 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
 python experiments/embedding_info_analysis.py  --dataset magna --weighted 0 --node-attributed 1 --undirected 1 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
 python experiments/embedding_info_analysis.py  --dataset enron_na --weighted 0 --node-attributed 1 --undirected 0 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
 python experiments/embedding_info_analysis.py  --dataset polblogs --weighted 0 --node-attributed 1 --undirected 0 --methods acc_pcapass_gnn_sv_spectra --dims 512 --num_epochs 200
